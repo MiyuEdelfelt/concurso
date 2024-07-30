9a8b548edf98954614a9f6c8cf91b18462cefb95
CarolaMine23 <guillermo.gordillo@mineracarola.cl>
2024-07-30 09:50:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import QRCode from 'qrcode.react';
+import './App.css'; 
 
 function App() {
+  const imageUrl = 'https://www.dropbox.com/scl/fi/gn3x5fgui028szhg7v9tr/bases-concurso.jpg?rlkey=lm0a0uhu31vjc6c16tvaq25vo&st=80f2y4sl&dl=0'; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bases Concurso</h1>
+      <div className="qr-container">
+        <QRCode value={imageUrl} size={256} /> 
+      </div>
     </div>
   );
 }
